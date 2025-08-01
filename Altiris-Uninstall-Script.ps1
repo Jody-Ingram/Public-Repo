e322f3b093016e254e1cbfcc1d02b6aebfd10686
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2025-08-01 00:06:37 -0400

    Create Altiris-Uninstall-Script.ps1

---
+++ Altiris-Uninstall-Script.ps1
@@ -0,0 +1,27 @@
+<#
+Script  :  Altiris-Uninstall-Script.ps1
+Version :  1.0
+Date    :  7/30/25
+Author: Jody Ingram
+Notes: This script imports the base/default install locations for Altiris/Symantec Endpoint Management Suite and then runs the removal tool against them.
+#>
+
+# This uses the default install paths and also assumes the app may be on other drives.
+$paths = @( 
+    "C:\Program Files\Altiris\Altiris Agent"
+    "D:\Program Files\Altiris\Altiris Agent"
+    "E:\Program Files\Altiris\Altiris Agent"
+    
+)
+
+foreach ($basePath in $paths) {
+    if (Test-Path "$basePath\AeXAgentUtil.exe") {
+        Start-Process -FilePath "$basePath\AeXAgentUtil.exe" -ArgumentList "/clean" -Wait -WindowStyle Hidden
+        Start-Process -FilePath "$basePath\AeXAgentUtil.exe" -ArgumentList "/uninstall" -Wait -WindowStyle Hidden
+        if (Test-Path "$basePath\AeXNSAgent.exe") {
+            Start-Process -FilePath "$basePath\AeXNSAgent.exe" -ArgumentList "/uninstall" -Wait -WindowStyle Hidden
+        }
+        Start-Process -FilePath "$basePath\AeXAgentUtil.exe" -ArgumentList "/uninstallagents /clean" -Wait -WindowStyle Hidden
+        break
+    }
+}
