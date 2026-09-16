86b2931f6fb8480ce95dd9b0598686a0e2198c74
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2026-09-16 09:45:55 -0400

    Import Active Directory module and define OU
    Added import statement for Active Directory module and specified the OU for retrieving computer objects.

---
+++ AD-GetADObjectListfromOU.ps1
@@ -0,0 +1,22 @@
+<#
+Script  :  AD-GetADObjectListfromOU.ps1
+Version :  1.0
+Date    :  9/16/26
+Pre-reqs: N/A
+Notes: This script retrieves a list of Active Directory computer objects from a specified Organizational Unit (OU).
+#>
+
+# Imports the module needed to run this script
+Import-Module ActiveDirectory
+
+# Specify the AD OU
+$OU = "OU=Servers,OU=Servers,DC=company,DC=com"
+
+Get-ADComputer -SearchBase $OU -SearchScope Subtree -Filter * `
+    -Properties DNSHostName, Enabled |
+    Sort-Object Name |
+    Select-Object Name, DNSHostName, Enabled, DistinguishedName
+
+# Uncomment if needed to export to .csv
+
+# Export-Csv "$env:USERPROFILE\Desktop\OU-Computers.csv" -NoTypeInformation
